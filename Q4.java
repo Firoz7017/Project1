7ed62023d5ca0cb6a53e2f39b7ab07b3227676d0
Firoz7017 <145679966+Firoz7017@users.noreply.github.com>
2023-09-21 21:45:24 +0530

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,10 @@
+
+public class Q4{
+public static void main(String[] args){
+String s="Take you forward";
+String s1="";
+s1=s.replaceAll(" ","");
+System.out.println("String after removing space::"+s1);
+
+}
+}
